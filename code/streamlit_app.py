87689629bd8eb68f26f9ccdf27711319ac608ffc
dmc747 <dcostello@hms.harvard.edu>
2023-03-05 19:43:43 -0500

    Modified load_data() to address Streamlit warning

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -4,55 +4,55 @@ import streamlit as st
 import numpy as np
 
 DATA_URL = ("https://raw.githubusercontent.com/dmc747/bmi706_project/main/data/nhanes_dementia_risk_factors.csv")
-@st.cache
+@st.cache_data
 def load_data():
     data = pd.read_csv(DATA_URL)
-    return data
+
+    # Create age categories: mid-life (45-64), and late-life (> 65yrs)
+    ages = [ (dementia_df['RIDAGEYR']<= 64), (dementia_df['RIDAGEYR'] >= 65)]
+    categories = ['mid_life', 'later_life']
+    # Use np.select to assign ages to categories 
+    dementia_df['Age'] = np.select(ages, categories)
+
+    # Output the list of risk factors 
+    riskfactors = dementia_df.columns[dementia_df.columns.str.contains('risk')] 
+
+    # Calculate total risk factors per person
+    dementia_df['Total risk factors'] = dementia_df[riskfactors].sum(axis=1)
+
+    # Create a list of total risk categories
+    riskcategory = [ (dementia_df['Total risk factors'] <= 1), 
+                    (dementia_df['Total risk factors'] > 1)  & (dementia_df['Total risk factors'] <= 3), 
+                    (dementia_df['Total risk factors'] > 3)]
+    riskname = ['less than 1', '2 and 3', 'greater than 3']
+    dementia_df['Total risk category'] = np.select(riskcategory,riskname)
+
+    # Rearrange the df with one risk factor column
+    dementia_df_new = pd.melt(
+        dementia_df, id_vars=['SEQN','RIAGENDR','RIDRETH1', 'DMDEDUC2','WTMEC2YR', 'SDMVPSU','SDMVSTRA','year','Age','Total risk factors','Total risk category'], value_vars=riskfactors, var_name='Risk factor', value_name='Present'
+    )
+
+    # Change numeric codes to character for Streamlit selections
+
+    # 1. Sex
+    # Create the dictionary and add sex column
+    gender_dictionary ={1 : 'male', 2 : 'female'}
+    dementia_df_new['Sex'] = dementia_df_new['RIAGENDR'].map(gender_dictionary)
+ 
+    # 2. Ethnicity
+    eth_dictionary = {1: 'Mexican American', 2:'Other Hispanic', 3:'Non-Hispanic White', 4:'Non-Hispanic Black', 5: 'Other'}
+    dementia_df_new['Ethnicity'] = dementia_df_new['RIDRETH1'].map(eth_dictionary)
+ 
+    # 3. Education
+    edu_dictionary = {1 : '<9th grade', 2: '9th-11th grade', 
+                    3 : 'high school diploma/GED/Equivalent', 4: 'some college or Associate’s degree', 
+                    5 : 'college graduate or higher', 7: 'Refused', 9: 'Don’t know'}
+    
+    dementia_df_new['Education'] = dementia_df_new['DMDEDUC2'].map(edu_dictionary)  
+    return dementia_df_new
 
 # Read in the data containing the risk factors for dementia
 dementia_df = load_data()
-
-# Create age categories: mid-life (45-64), and late-life (> 65yrs)
-ages = [ (dementia_df['RIDAGEYR']<= 64), (dementia_df['RIDAGEYR'] >= 65)]
-categories = ['mid_life', 'later_life']
-# Use np.select to assign ages to categories 
-dementia_df['Age'] = np.select(ages, categories)
-
-# Output the list of risk factors 
-riskfactors = dementia_df.columns[dementia_df.columns.str.contains('risk')] 
-
-# Calculate total risk factors per person
-dementia_df['Total risk factors'] = dementia_df[riskfactors].sum(axis=1)
-
-# Create a list of total risk categories
-riskcategory = [ (dementia_df['Total risk factors'] <= 1), 
-                (dementia_df['Total risk factors'] > 1)  & (dementia_df['Total risk factors'] <= 3), 
-                (dementia_df['Total risk factors'] > 3)]
-riskname = ['less than 1', '2 and 3', 'greater than 3']
-dementia_df['Total risk category'] = np.select(riskcategory,riskname)
-
-# Rearrange the df with one risk factor column
-dementia_df_new = pd.melt(
-     dementia_df, id_vars=['SEQN','RIAGENDR','RIDRETH1', 'DMDEDUC2','WTMEC2YR', 'SDMVPSU','SDMVSTRA','year','Age','Total risk factors','Total risk category'], value_vars=riskfactors, var_name='Risk factor', value_name='Present'
-)
-
-# Change numeric codes to character for Streamlit selections
-
-# 1. Sex
-# Create the dictionary and add sex column
-gender_dictionary ={1 : 'male', 2 : 'female'}
-dementia_df_new['Sex'] = dementia_df_new['RIAGENDR'].map(gender_dictionary)
- 
-# 2. Ethnicity
-eth_dictionary = {1: 'Mexican American', 2:'Other Hispanic', 3:'Non-Hispanic White', 4:'Non-Hispanic Black', 5: 'Other'}
-dementia_df_new['Ethnicity'] = dementia_df_new['RIDRETH1'].map(eth_dictionary)
- 
-# 3. Education
-edu_dictionary = {1 : '<9th grade', 2: '9th-11th grade', 
-                  3 : 'high school diploma/GED/Equivalent', 4: 'some college or Associate’s degree', 
-                  5 : 'college graduate or higher', 7: 'Refused', 9: 'Don’t know'}
-    
-dementia_df_new['Education'] = dementia_df_new['DMDEDUC2'].map(edu_dictionary)  
 
 # Visualization Tasks
 # 
@@ -66,8 +66,8 @@ st.write("## Key dementia risk factors in different populations")
 
 # Task 1: line chart: risk factor prevalence over time
 # 1.1 prepare df and calculate prevalence
-df1 = dementia_df_new.groupby(['year','Risk factor']).size().reset_index(name='population')
-df1_1 = dementia_df_new.groupby(['year','Risk factor','Present']).size().reset_index(name='count')
+df1 = dementia_df.groupby(['year','Risk factor']).size().reset_index(name='population')
+df1_1 = dementia_df.groupby(['year','Risk factor','Present']).size().reset_index(name='count')
 df1_new = df1_1.merge(df1, on=['year', 'Risk factor'], how ='left')
 df1_new['Prevalence'] = df1_new.query("Present==1")['count']/df1_new['population']*100
 df1_new.dropna(inplace=True)
@@ -119,8 +119,8 @@ chart1
 # Task 2: Prevalence of dementia risk factors by sex 
 
 # 2.1 calculate prevalence by sex, risk factor, and year
-df2 = dementia_df_new.groupby(['year','Sex','Risk factor']).size().reset_index(name='population')
-df2_1 = dementia_df_new.groupby(['year','Risk factor','Sex','Present']).size().reset_index(name='count')
+df2 = dementia_df.groupby(['year','Sex','Risk factor']).size().reset_index(name='population')
+df2_1 = dementia_df.groupby(['year','Risk factor','Sex','Present']).size().reset_index(name='count')
 df2_new = df2_1.merge(df2, on=['year', 'Risk factor','Sex'], how ='left')
 df2_new['Prevalence'] = df2_new.query("Present==1")['count']/df2_new['population']*100
 df2_new.dropna(inplace=True)
@@ -162,8 +162,8 @@ bar
 # Task 3: Prevalence of dementia risk factors by ethnicity by year
 
 # 3.1 calculate prevalence by ethnicity, risk factor, and year
-df3 = dementia_df_new.groupby(['year','Ethnicity','Risk factor']).size().reset_index(name='population')
-df3_1 = dementia_df_new.groupby(['year','Risk factor','Ethnicity','Present']).size().reset_index(name='count')
+df3 = dementia_df.groupby(['year','Ethnicity','Risk factor']).size().reset_index(name='population')
+df3_1 = dementia_df.groupby(['year','Risk factor','Ethnicity','Present']).size().reset_index(name='count')
 df3_new = df3_1.merge(df3, on=['year', 'Risk factor','Ethnicity'], how ='left')
 df3_new['Prevalence'] = df3_new.query("Present==1")['count']/df3_new['population']*100
 df3_new.dropna(inplace=True)
@@ -205,8 +205,8 @@ bar_ethnicity
 # Task 4: Prevalence of dementia risk factors by age group (mid-life and later life) by year 
 
 # 4.1 calculate prevalence by Age group, risk factor, and year
-df4 = dementia_df_new.groupby(['year','Age','Risk factor']).size().reset_index(name='population')
-df4_1 = dementia_df_new.groupby(['year','Risk factor','Age','Present']).size().reset_index(name='count')
+df4 = dementia_df.groupby(['year','Age','Risk factor']).size().reset_index(name='population')
+df4_1 = dementia_df.groupby(['year','Risk factor','Age','Present']).size().reset_index(name='count')
 df4_new = df4_1.merge(df4, on=['year', 'Risk factor','Age'], how ='left')
 df4_new['Prevalence'] = df4_new.query("Present==1")['count']/df4_new['population']*100
 df4_new.dropna(inplace=True)
@@ -249,8 +249,8 @@ bar_age
 # Task 5: Prevalence of dementia risk factors by education by year 
 
 # 5.1 calculate prevalence in each combination of education, risk factor, and year
-df5 = dementia_df_new.groupby(['year','Education','Risk factor']).size().reset_index(name='population')
-df5_1 = dementia_df_new.groupby(['year','Risk factor','Education','Present']).size().reset_index(name='count')
+df5 = dementia_df.groupby(['year','Education','Risk factor']).size().reset_index(name='population')
+df5_1 = dementia_df.groupby(['year','Risk factor','Education','Present']).size().reset_index(name='count')
 df5_new = df5_1.merge(df5, on=['year', 'Risk factor','Education'], how ='left')
 df5_new['Prevalence'] = df5_new.query("Present==1")['count']/df5_new['population']*100
 # data cleanup
@@ -306,8 +306,8 @@ bar_edu
 # Task 6: Prevalence of multiple risk factors in mid-life and later-life adults by year 
 
 # 6.1 calculate prevalence in each combination of multiple risk factor, age group, and year
-df6 = dementia_df_new.groupby(['year','Age']).size().reset_index(name='population')
-df6_1 = dementia_df_new.groupby(['year','Age','Total risk category']).size().reset_index(name='count')
+df6 = dementia_df.groupby(['year','Age']).size().reset_index(name='population')
+df6_1 = dementia_df.groupby(['year','Age','Total risk category']).size().reset_index(name='count')
 df6_new = df6_1.merge(df6, on=['year', 'Age'], how ='left')
 df6_new['Prevalence'] = df6_new['count']/df6_new['population']*100
 df6_new.dropna(inplace=True) 
